af329eee76d153d66b56a77e5cca8669d9206a51
Gaurav Singh <91187114+GS-GauravSingh@users.noreply.github.com>
2024-04-18 18:06:30 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'nunito': ["Nunito"],
+      },
+    },
+  },
+  plugins: [],
+}
+
